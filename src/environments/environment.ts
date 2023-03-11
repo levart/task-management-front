b21df7608ce5c13d8464cb5835d4108516aff5a4
levan jmukhadze <jmukhadze@gmail.com>
2023-03-11 12:52:40 +0400

    ci/cd

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-  production: false,
-  apiUrl: 'http://localhost:3000/api/',
+  production: true,
+  apiUrl: 'https://task-manager-production-b954.up.railway.app/api/',
 };
